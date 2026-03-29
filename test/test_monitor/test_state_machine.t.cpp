951d576b071f99b21b9b5269cfaf84811be3923f
Ashish Krishnan <ashishkrish09@gmail.com>
2026-03-29 14:01:33 +0530

    Add test_state_machine.t.cpp for state transition verification

---
+++ test/test_monitor/test_state_machine.t.cpp
@@ -0,0 +1,41 @@
+#include <unity.h>
+#include "app/monitor.h"
+#include "test_utils.h"
+
+void test_charging_state_detection() {
+    MockSensor mock;
+    MockTemperature mockTemp;
+    Monitor monitor(&mock, &mockTemp);
+
+    // Normal discharge
+    mock.mockVoltage = 12.0;
+    mock.mockCurrent = -5.0;
+    mock.mockPower = -60.0;
+    monitor.update(0);
+    TEST_ASSERT_EQUAL(POWER_STATE_CHARGING, monitor.getDTO().power_state);
+    
+    // Switch to charging
+    mock.mockVoltage = 13.5;
+    mock.mockCurrent = 2.0;
+    mock.mockPower = 27.0;
+    monitor.update(250);
+    
+    TEST_ASSERT_EQUAL(POWER_STATE_CHARGING, monitor.getDTO().power_state);
+    TEST_ASSERT_EQUAL(ALERT_LEVEL_NONE, monitor.getDTO().alert_level);
+}
+
+void test_on_battery_safe_state() {
+    MockSensor mock;
+    MockTemperature mockTemp;
+    Monitor monitor(&mock, &mockTemp);
+
+    // Normal discharge (not critical or warning)
+    mock.mockVoltage = 12.5;
+    mock.mockCurrent = -5.0;
+    mock.mockPower = -62.5;
+    monitor.update(0);
+    
+    TelemetryDTO& dto = monitor.getDTO();
+    TEST_ASSERT_EQUAL(POWER_STATE_ON_BATTERY, dto.power_state);
+    TEST_ASSERT_EQUAL(ALERT_LEVEL_NONE, dto.alert_level);
+}
